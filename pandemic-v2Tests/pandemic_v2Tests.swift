47e1c6892d4524d5ad69c340c3a0ae98502d5d4e
akyle9 <akyle@andrew.cmu.edu>
2014-09-26 22:51:13 -0400

    Initial Commit

---
+++ pandemic-v2Tests/pandemic_v2Tests.swift
@@ -0,0 +1,36 @@
+//
+//  pandemic_v2Tests.swift
+//  pandemic-v2Tests
+//
+//  Created by Akira Kyle on 9/26/14.
+//  Copyright (c) 2014 Akira Kyle. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class pandemic_v2Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
